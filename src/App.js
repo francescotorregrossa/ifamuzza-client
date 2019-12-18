940a2d2ec3f35d653a9fbd5efaa28f1e8963c9bb
Francesco Torregrossa <d.ftorregrossa@gmail.com>
2019-12-18 11:37:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          
+        </SafeAreaView>
+      </>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
