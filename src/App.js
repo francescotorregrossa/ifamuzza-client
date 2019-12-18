92a4dc1d9207bb1872e707aa22bc9c852421d9ed
Michele Sanfilippo <michelesanfilippo01@gmail.com>
2019-12-18 13:57:53 +0100

    inizio homepage

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ import {
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
 } from 'react-native';
+import { 
+  Button,
+  ButtonGroup,
+  Icon
+} from 'react-native-elements';
 
 class App extends React.Component {
   render() {
+
     return (
       <>
         <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          
+        <SafeAreaView style={styles.container}>
+          <Button style={styles.mainbutton}
+            icon={
+            <Icon 
+            name='sign-in-alt' 
+            type='fontawesome'
+            color='white' />
+
+            }
+            
+          />
+
         </SafeAreaView>
       </>
     );
@@ -22,7 +39,18 @@ class App extends React.Component {
 };
 
 const styles = StyleSheet.create({
-  
+   container: {
+     alignItems: 'center',
+     justifyContent: 'center',
+     backgroundColor: '#ecf0f1',
+   },
+   mainbutton: {
+    flex: 1,
+    flexDirection: 'row-reverse',
+    height: 50,
+    width: 50,
+
+   }
 });
 
 export default App;
