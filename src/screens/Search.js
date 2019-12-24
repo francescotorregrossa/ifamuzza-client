6ad076bdf49d3e0f32245d0f622c0e43c576d2fe
Michele Sanfilippo <michelesanfilippo01@gmail.com>
2019-12-24 17:43:37 +0100

    Search.js con richiesta remota al server e visualizzazione elementi con flatlist

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
 import {Button} from 'react-native-elements';
+import {ListItem} from 'native-base';
 import Auth from '../model/Auth';
 import pages from '../pages';
 
@@ -8,13 +9,58 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: props.navigation.state.params,
+      loading: false,
+      data: [],
+      seed: 1,
+      page: 1,
+      error: null,
+      refreshing: false,
     };
   }
 
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const {page, seed} = this.state;
+    const url =
+      'http://127.0.0.1:8080/api/search?address=Via%20Giuseppe%20Patricolo,%206,%20Palermo'; // link api
+    this.setState({loading: true});
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false,
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error,
+          loading: false,
+        });
+      });
+  };
+
   render() {
-    console.log('suca', this.state.city);
-    return null;
+    return (
+      <>
+        <FlatList
+          // data={this.state.data}
+          renderItem={({item}) => (
+            <ListItem
+              roundAvatar
+              title={'${item.name.first}'}
+              avatar={{uri: item.picture.thumbnail}} // info sui locali presi dall'api, modificare se necessario
+            />
+          )}
+          keyExtractor={item => this.state.data[item].id}
+        />
+      </>
+    );
   }
 }
 
