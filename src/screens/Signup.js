5f7a97d2d0a4831b5e7e38d982f90136a2fa77a7
DavideAvellone <58786401+DavideAvellone@users.noreply.github.com>
2019-12-26 19:26:31 +0100

    aggiunte label,sistemate icone e pulsante e eliminato titolo e fine

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -12,7 +12,6 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import pages from '../pages';
 
 class Signup extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -33,32 +32,38 @@ class Signup extends React.Component {
       <>
         <SafeAreaView style={styles.container}>
           <ScrollView>
-            <View>
-              <Text style={{fontSize: 25, textAlign: 'center'}}>
-                Register on{' '}
-                <Text
-                  style={{color: 'blue', fontWeight: 'bold'}}
-                  onPress={() => this.props.navigation.popToTop()}>
-                  iFamuzza{' '}
-                </Text>
-              </Text>
-            </View>
             <Input
               placeholder="Enter your email"
+              label="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="Enter a valid email"
               leftIcon={{
                 type: 'font-awesome',
                 name: 'envelope',
                 color: 'gray',
-                size: 19, 
                 containerStyle: {marginRight: 16},
-              }}          
+              }}
               // onChangeText={this.updateSearch}
               // value={this.state.insert}
             />
             <Input
               placeholder="Enter your password"
+              label="Password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="Enter a valid password"
               // onChangeText={this.updateSearch}
@@ -68,11 +73,20 @@ class Signup extends React.Component {
                 type: 'font-awesome',
                 name: 'lock',
                 color: 'gray',
+                size: 30,
                 containerStyle: {marginRight: 16},
-              }}        
+              }}
             />
             <Input
               placeholder="Confirm your password"
+              label="Confirm password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="The password must be the same as the one in the previous field"
               // onChangeText={this.updateSearch}
@@ -82,12 +96,21 @@ class Signup extends React.Component {
                 type: 'font-awesome',
                 name: 'lock',
                 color: 'gray',
+                size: 30,
                 containerStyle: {marginRight: 16},
-              }}        
+              }}
             />
             <Text style={{padding: 10, fontSize: 20}}> Optional </Text>
             <Input
               placeholder="Name"
+              label="Name"
+              autoCapitalize="words"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="This field can only contain letters (Max 20)"
               // onChangeText={this.updateSearch}
@@ -95,6 +118,14 @@ class Signup extends React.Component {
             />
             <Input
               placeholder="Surname"
+              label="Surname"
+              autoCapitalize="words"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="This field can only contain letters (Max 20)"
               // onChangeText={this.updateSearch}
@@ -102,6 +133,13 @@ class Signup extends React.Component {
             />
             <Input
               placeholder="Phone"
+              label="Phone"
+              keyboardType="phone-pad"
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="This field can only contain numbers (Exactly 10)"
               // onChangeText={this.updateSearch}
@@ -109,6 +147,14 @@ class Signup extends React.Component {
             />
             <Input
               placeholder="Allergies"
+              label="Allergies"
+              autoCapitalize="none"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage=""
               // onChangeText={this.updateSearch}
@@ -116,6 +162,15 @@ class Signup extends React.Component {
             />
             <Input
               placeholder="Shipping address"
+              label="Shipping address"
+              autoCapitalize="words"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+                marginTop: 13,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="Invalid shipping address"
               // onChangeText={this.updateSearch}
@@ -123,22 +178,29 @@ class Signup extends React.Component {
             />
             <Input
               placeholder="Payment method"
+              label="Payment method"
+              autoCapitalize="on"
+              autoCorrect={false}
+              labelStyle={{
+                color: 'black',
+                fontSize: 20,
+                marginLeft: 3,
+              }}
               errorStyle={{color: 'red'}}
               errorMessage="Invalid payment method"
               // onChangeText={this.updateSearch}
               // value={this.state.insert}
             />
             <View style={{borderRadius: 70, padding: 10}}>
-              <Button title="Create account" type="solid" />
+              <Button
+                title="Create account"
+                type="solid"
+                titleStyle={{
+                  fontWeight: 'bold',
+                  fontSize: 20,
+                }}
+              />
             </View>
-            <Text style={styles.text2}>
-              Are you already registered on iFamuzza?
-              <Text
-                style={styles.underline}
-                onPress={() => this.props.navigation.goBack()}>
-                Login{' '}
-              </Text>
-            </Text>
           </ScrollView>
         </SafeAreaView>
       </>
